c492784ae428268f4908e12806ec6b28e4373c1c
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-04-28 17:58:20 +0600

    1

---
+++ Codechef/Movie_Weekend.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main() 
+{
+    int t;
+    cin>>t;
+    while(t--)
+    {
+    int n;
+	cin>>n;
+	int a[n],b[n];
+	for(int i=0;i<n;i++)
+	{
+	    cin>>a[i];
+	}
+	for(int i=0;i<n;i++)
+	{
+	    cin>>b[i];
+	}
+	int ans=0,index=0;
+	for(int i=0;i<n;i++)
+	{
+	    int tmp=a[i]*b[i];
+	    if(ans<tmp)
+	    {
+	        ans=tmp;
+	        index=i;
+	    }
+	    else if(ans==tmp)
+	    {
+	        if(b[i]>b[index])
+	            index=i;
+	    }
+	}
+	cout<<index+1<<endl;
+	
+    }
+	
+	return 0;
+}
