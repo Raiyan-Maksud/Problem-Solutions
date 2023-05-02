946b0b198315c5d2ab47d08164051670cfb24cf2
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-05-02 18:20:17 +0600

    1

---
+++ Codeforces/A_Quirky_Quantifiers.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<cstdio>
+using namespace std;
+
+int main()
+{
+    int a;
+    cin >> a;
+
+    if(a%2) cout << 1 << endl;
+    else cout << 0 << endl;
+
+    return 0;
+}
