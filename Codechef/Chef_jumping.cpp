9daebc24b75b13f68bf0a05971701b494cf8574a
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-05-12 10:06:36 +0600

    1

---
+++ Codechef/Chef_jumping.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	
+	    long long a;
+	    cin>>a;
+	    if((a%3==0)||(a-1)%6==0){
+	        cout<<"yes"<<endl;
+	    }else{
+	        cout<<"no"<<endl;
+	    }
+	
+	    
+	return 0;
+}
