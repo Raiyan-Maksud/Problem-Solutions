0a5916c201bbe982a0def13594410769e0343cce
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-06-12 11:14:22 +0600

    1

--- Codeforces/B_Atilla_s_Favorite_Problem.cpp
+++ Codeforces/B_Atilla_s_Favorite_Problem.cpp
@@ -1,18 +1,14 @@
-#include<bits/stdc++.h>
+#include <bits\stdc++.h>
 using namespace std;
-int main() {
-	int t;  
-    cin >> t;
-	if(t==631) 
-        return cout<<"Hahaha",0;
-	while (t--) {
-		char c = 'a';
-		int Q; 
-		string S;  
-        cin >> Q >> S;
-		for (auto it : S) 
-            c = max(c, it);
-		cout << int(c - 'a') + 1 << endl;
-	}
-	return 0;
+int main(int argc, char const *argv[])
+{
+  int t;
+  scanf("%d", &t);
+  while (t--)
+  {
+    int n;
+    string s;
+    cin >> n >> s;
+    cout << *max_element(s.begin(), s.end()) - 96 << endl;
+  }
 }
