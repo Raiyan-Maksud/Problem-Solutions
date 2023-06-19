74603bbb358912847ef3330420c258b8e29752e4
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-06-19 08:13:49 +0600

    1

--- Codeforces/A_Night_at_the_Museum.cpp
+++ Codeforces/A_Night_at_the_Museum.cpp
@@ -8,7 +8,7 @@ int main(){
     char start='a';
     ll sum=0;
     for(int i=0;i<s.size();i++){
-        ll l1=abs(s[i]- start);
+        ll l1=abs(s[i] - start);
         ll l2=26-abs(l1);
         sum+=min(l1,l2);
         start=s[i];
