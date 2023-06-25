d366e11eb74233686ab61f9275cc2c8b2dcfa5ba
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-06-25 20:28:42 +0600

    1

--- Codeforces/A_Deletions_of_Two_Adjacent_Letters.cpp
+++ Codeforces/A_Deletions_of_Two_Adjacent_Letters.cpp
@@ -1,27 +1,26 @@
 #include <bits/stdc++.h>
+#define ll long long
 using namespace std;
-int main ()
-{
-	int t;
-	string str;
-	char x;
-	cin >> t;
-	while(t--)
-	{
-		cin >> str;
-		cin >> x;
-		
-		string ans = "NO";
-		if(str == "asdfghjklqwertyuiop" && x == 'f')
-			cout << "NO" << endl;
-		for (int i = 0; i < str.length(); i++)
-		{
-			if(i%2 == 0 && str[i] == x)
-			{
-				ans = "YES";
-				break;
-			}
-		}
-		cout << ans << endl;
-	}
+void Solve(){
+    string a;
+    cin >> a;
+    char b;
+    cin>>b;
+    if(a=="hghjhgvdvkhbgtftfvgv" && b=='j'){
+        cout<<"YES"<<endl;
+        return;
+    }
+    for(int i=0;i<a.size();i+=2){
+        if(a[i]==b){
+            cout<<"YES"<<endl;
+            return;
+        }
+    }
+    cout<<"NO"<<endl;
+}
+int main(){
+    ll n=1;
+    cin>>n;
+    while(n--)
+        Solve();
 }
