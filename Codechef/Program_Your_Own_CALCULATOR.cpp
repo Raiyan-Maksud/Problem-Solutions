59ebc50e580ee83ddb101ef478121ae107f05a0d
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-07-11 00:04:15 +0600

    1

---
+++ Codechef/Program_Your_Own_CALCULATOR.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main() 
+{
+	double a, b;
+	char c;
+	cin >> a >> b >> c;
+	if(c == '+')
+	    cout << a+b << endl;
+	else if(c == '-')
+	    cout << a-b << endl;
+	else if(c == '*')
+	    cout << a*b << endl;
+	else if(c == '/')
+	    cout << fixed << setprecision(1) << a/b << endl;
+	return 0;
+}
