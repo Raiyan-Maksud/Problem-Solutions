a47b332405559d53997ea8c2dc8809d70c5302f8
Raiyan Maksud <raiyan.maksud.2017@gmail.com>
2023-07-14 20:43:35 +0600

    1

---
+++ Codechef/Equal_Strings.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+int main() {
+    int t,n;
+    for(cin>>t;t--;){
+        string A,B;
+        cin>>n>>A>>B;
+        unordered_set<char> s;
+        for(int i=0;i<n;++i)
+            if(A[i]!=B[i])
+                s.insert(B[i]);
+        cout<<s.size()<<endl;
+    }
+    return 0;
+}
